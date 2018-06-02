8d7d576713436f8d1f3beeb7c2237fcdaf03d3a9
CatarinaFrost <36791485+CatarinaFrost@users.noreply.github.com>
2018-06-03 01:56:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,27 +6,27 @@ include_once("bd.php");
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Chek in</title>
+	<title>Web-compiler</title>
 	<link rel="stylesheet" href="index.css">
 </head>
 <body>
-	<h2>SEA BUTTLESHIP</h2>
+	<h2>Web-compiler</h2>
 	<form action="login.php" method="post">
 		<label>Login: </label><br>
-		<input placeholder="Mr_Frosty" type="text" name="login" size="20" required/><br><br>
+		<input type="text" name="login" size="20" required/><br><br>
 		<label class="pass" >Password: </label><br>
-		<input placeholder="12345" type="password" name="pass" size="20" required><br> 
+		<input type="password" name="pass" size="20" required><br> 
 		<td colspan="2"><input style="margin-top: 15px;" class="submit" type="submit" name="submit" value="Log in">
 		</form>
 		<br>
 
-		<label class="time" >For the first time on Sea Buttleship?</label>
+		<label class="time" >For the first time on Web-compiler?</label>
 		<form action="registration.php" method="post">
 		<input class="submit" type="submit" name="submit" value="Sign in">
 		</form>
 
-		<form action="game.php" method="post">
-		<input class="submit1" type="submit" value="Play without registration!">
+		<form action="compiler.php" method="post">
+		<input class="submit1" type="submit" value="without registration!">
 		</form>
 	</form>
 </body>
