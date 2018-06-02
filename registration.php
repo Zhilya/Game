2a8dcbcf4869bb7dd8d741538c78b94af9d47b53
CatarinaFrost <36791485+CatarinaFrost@users.noreply.github.com>
2018-06-03 02:42:36 +0300

    Регистрация

---
+++ registration.php
@@ -0,0 +1,29 @@
+<?php 	
+include_once("bd.php");
+?>
+
+<!DOCTYPE html>
+</html lang="en">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Create your account</title>
+    <link rel="stylesheet" href="registration.css">
+</head>
+<body>
+    <h2>Create your account</h2>
+    <form action="form.php" method="post">
+        <p>
+            <label>Login:<br></label>
+            <input class="submit1" placeholder="Mr_Frosty" name="login" type="text" size="20" maxlength="20">
+        </p>
+        <p>
+            <label>Password:<br></label>
+            <input class="submit1" placeholder="12345" name="pass" type="password" size="20" maxlength="20">
+        </p>
+        <p>
+            <input class="submit" type="submit" name="submit" value="SIGN UP">
+        </p>
+    </form>
+</body>
+</html>
