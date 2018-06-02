6c78593bf7ecec93a95dbd4e894e5545a6885d5c
CatarinaFrost <36791485+CatarinaFrost@users.noreply.github.com>
2018-06-03 02:44:12 +0300

    Update registration.php
    убрала ненужное

--- registration.php
+++ registration.php
@@ -15,11 +15,11 @@ include_once("bd.php");
     <form action="form.php" method="post">
         <p>
             <label>Login:<br></label>
-            <input class="submit1" placeholder="Mr_Frosty" name="login" type="text" size="20" maxlength="20">
+            <input class="submit1" name="login" type="text" size="20" maxlength="20">
         </p>
         <p>
             <label>Password:<br></label>
-            <input class="submit1" placeholder="12345" name="pass" type="password" size="20" maxlength="20">
+            <input class="submit1" name="pass" type="password" size="20" maxlength="20">
         </p>
         <p>
             <input class="submit" type="submit" name="submit" value="SIGN UP">
